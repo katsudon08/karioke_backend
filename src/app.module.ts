a83bece4851544904d921aa2f3d9dca93ebf53a5
matsu <matsuaki171101@gmail.com>
2023-11-15 16:28:05 +0900

    テスト用に追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { SongModule } from './song/song.module';
 import { CorsMiddleware } from './cors.middleware';
+import { AppController } from './app.controller';
 
 @Module({
     imports: [SongModule],
-    controllers: [],
+    controllers: [AppController],
     providers: [],
 })
 export class AppModule implements NestModule {
